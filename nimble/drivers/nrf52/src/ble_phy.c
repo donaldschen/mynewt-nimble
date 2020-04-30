3835a7d69917aadc3158f69e47dde5eaa11138c4
Donald Chen <donald.chen@rigado.com>
2020-04-29 17:04:46 -0700

    pa/lna support

--- nimble/drivers/nrf52/src/ble_phy.c
+++ nimble/drivers/nrf52/src/ble_phy.c
@@ -28,6 +28,7 @@
 #include "nimble/nimble_npl.h"
 #include "controller/ble_phy.h"
 #include "controller/ble_phy_trace.h"
+#include "controller/ble_phy_palna.h"
 #include "controller/ble_ll.h"
 #include "nrfx.h"
 #if MYNEWT
@@ -799,6 +800,10 @@ ble_phy_rx_xcvr_setup(void)
     }
 #endif
 
+#if (MYNEWT_VAL(BLE_PHY_PALNA_PA_ENABLE_PIN) || MYNEWT_VAL(BLE_PHY_PALNA_LNA_ENABLE_PIN))
+    ble_phy_palna_rx_prepare();
+#endif
+
     /* Turn off trigger TXEN on output compare match and AAR on bcmatch */
     NRF_PPI->CHENCLR = PPI_CHEN_CH20_Msk | PPI_CHEN_CH23_Msk;
 
@@ -869,6 +874,10 @@ ble_phy_tx_end_isr(void)
     NRF_RADIO->EVENTS_END = 0;
     wfr_time = NRF_RADIO->SHORTS;
     (void)wfr_time;
+
+#if (MYNEWT_VAL(BLE_PHY_PALNA_PA_ENABLE_PIN) || MYNEWT_VAL(BLE_PHY_PALNA_LNA_ENABLE_PIN))
+    ble_phy_palna_idle();
+#endif
 
 #if MYNEWT_VAL(BLE_LL_CFG_FEAT_LE_ENCRYPTION)
     /*
@@ -969,6 +978,10 @@ ble_phy_rx_end_isr(void)
 
     /* Disable automatic RXEN */
     NRF_PPI->CHENCLR = PPI_CHEN_CH21_Msk;
+
+#if (MYNEWT_VAL(BLE_PHY_PALNA_PA_ENABLE_PIN) || MYNEWT_VAL(BLE_PHY_PALNA_LNA_ENABLE_PIN))
+    ble_phy_palna_idle();
+#endif
 
     /* Set RSSI and CRC status flag in header */
     ble_hdr = &g_ble_phy_data.rxhdr;
@@ -1478,6 +1491,10 @@ ble_phy_init(void)
 
     ble_phy_dbg_time_setup();
 
+#if (MYNEWT_VAL(BLE_PHY_PALNA_PA_ENABLE_PIN) || MYNEWT_VAL(BLE_PHY_PALNA_LNA_ENABLE_PIN))
+    ble_phy_palna_init();
+#endif
+
     return 0;
 }
 
@@ -1688,6 +1705,10 @@ ble_phy_tx(ble_phy_tx_pducb_t pducb, void *pducb_arg, uint8_t end_trans)
      * it is moving to disabled state. If so, let it get there.
      */
     nrf_wait_disabled();
+
+#if (MYNEWT_VAL(BLE_PHY_PALNA_PA_ENABLE_PIN) || MYNEWT_VAL(BLE_PHY_PALNA_LNA_ENABLE_PIN))
+    ble_phy_palna_tx_prepare();
+#endif
 
     /*
      * XXX: Although we may not have to do this here, I clear all the PPI
@@ -1979,6 +2000,10 @@ ble_phy_disable(void)
 
     ble_phy_stop_usec_timer();
     ble_phy_disable_irq_and_ppi();
+
+#if (MYNEWT_VAL(BLE_PHY_PALNA_PA_ENABLE_PIN) || MYNEWT_VAL(BLE_PHY_PALNA_LNA_ENABLE_PIN))
+    ble_phy_palna_idle();
+#endif
 }
 
 /* Gets the current access address */
@@ -2085,3 +2110,4 @@ ble_phy_rfclk_disable(void)
     NRF_CLOCK->TASKS_HFCLKSTOP = 1;
 #endif
 }
+
