fa1f02354534db4aa15d1680d69accb79c480897
Donald Chen <donald.chen@rigado.com>
2020-07-01 17:26:31 -0700

    wip

--- apps/blehci/src/main.c
+++ apps/blehci/src/main.c
@@ -19,10 +19,36 @@
 
 #include <assert.h>
 #include "os/mynewt.h"
+#include "hal/hal_gpio.h"
+#include "controller/ble_ll.h"
+
+static void bmd345_init(void) {
+    //set the mac
+    #define BMD345_UICR_MAC_ADDR 0x10001080
+    ble_ll_set_public_addr((void*)BMD345_UICR_MAC_ADDR);
+
+    // enable the pa/lna
+    // p1.02
+    #define BLE_PHY_PALNA_ANTENNA_SELECT_PIN        34
+    #define BLE_PHY_PALNA_ANTENNA_SELECT_INTERNAL   1
+    #define BLE_PHY_PALNA_ANTENNA_SELECT_EXTERNAL   0
+
+    // p1.04
+    #define BLE_PHY_PALNA_MODE_PIN                  36
+    #define BLE_PHY_PALNA_MODE_ENABLE               0
+
+    // enable palna
+    hal_gpio_init_out(BLE_PHY_PALNA_MODE_ENABLE, BLE_PHY_PALNA_MODE_ENABLE);
+
+    // internal antenna
+    hal_gpio_init_out(BLE_PHY_PALNA_ANTENNA_SELECT_PIN, BLE_PHY_PALNA_ANTENNA_SELECT_INTERNAL);
+}
 
 int
 main(void)
 {
+    bmd345_init();
+
     /* Initialize OS */
     sysinit();
 
@@ -31,3 +57,5 @@ main(void)
     }
     return 0;
 }
+
+
